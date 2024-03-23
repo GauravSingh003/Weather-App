81c4f4467514702e8be55fcbf84dd9f654c58741
Gaurav Singh <145368335+GauravSingh003@users.noreply.github.com>
2024-03-24 03:02:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// function getWeather(){
+// 	var apiKey = '4c9d75a08d543fa23beb9e7615ddaa05';
+// 	var city = $('#BHOPAL').val();
+
+// 	$.get('https://home.openweathermap.org/api_keys')
+// }
+
+$(document).ready function(){
+
+}
